41291d66cad20fb61ca2531da42adfe5452bc640
Venilton Junior <venilton.junior.dev@gmail.com>
2026-01-22 04:22:27 -0300

    refactor: improve error handling in LoginUsecase

--- src/usecases/LoginUsecase.js
+++ src/usecases/LoginUsecase.js
@@ -1,5 +1,5 @@
 import { UserRepository } from '../repositories';
-import { BadRequestError } from '../utils';
+import { BadRequestError, NotFoundError } from '../utils';
 
 export class LoginUsecase {
   repository = new UserRepository();
@@ -7,7 +7,8 @@ export class LoginUsecase {
   async execute(email, password) {
     try {
       const user = await this.repository.findByEmail(email);
-      if (!user || user.password !== password) throw new BadRequestError('Invalid credentials');
+      if (!user) throw new NotFoundError('User not found');
+      if (user.password !== password) throw new BadRequestError('Invalid credentials');
   
       return user;
     }
