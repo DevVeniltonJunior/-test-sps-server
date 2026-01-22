c40cc55113a864d9a1dff50387368e343317a154
Venilton Junior <venilton.junior.dev@gmail.com>
2026-01-22 07:00:37 -0300

    fix: update PORT variable usage for server listening and logging

--- src/core/config/index.js
+++ src/core/config/index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import routes from "./routes.js";
 import cors from "cors";
-
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
